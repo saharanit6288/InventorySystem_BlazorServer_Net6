913232afcbb74bb8c9f787f104752aef6aa07ada
Raanit Saaha <saharanit6288@hotmail.com>
2023-09-29 22:52:20 +0530

    ASPNET Identity Added

---
+++ IMS.Plugins/IMS.Plugins.EFCoreSqlServer/IMSContext.cs
@@ -0,0 +1,60 @@
+﻿using IMS.CoreBusiness;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Plugins.EFCoreSqlServer
+{
+    public class IMSContext : DbContext
+    {
+        public IMSContext(DbContextOptions<IMSContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Inventory> Inventories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductInventory> ProductInventories { get; set; }
+        public DbSet<InventoryTransaction> InventoryTransactions { get; set; }
+        public DbSet<ProductTransaction> ProductTransactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductInventory>()
+                .HasKey(pi => new { pi.ProductId, pi.InventoryId });
+
+            modelBuilder.Entity<ProductInventory>()
+                .HasOne(pi => pi.Product)
+                .WithMany(p => p.ProductInventories)
+                .HasForeignKey(pi => pi.ProductId);
+
+            modelBuilder.Entity<ProductInventory>()
+                .HasOne(pi => pi.Inventory)
+                .WithMany(i => i.ProductInventories)
+                .HasForeignKey(pi => pi.InventoryId);
+
+            //seeding data
+            modelBuilder.Entity<Inventory>().HasData(
+                new Inventory { InventoryId = 1, InventoryName = "Bike Seat", Quantity = 10, Price = 2 },
+                new Inventory { InventoryId = 2, InventoryName = "Bike Body", Quantity = 10, Price = 15 },
+                new Inventory { InventoryId = 3, InventoryName = "Bike Wheel", Quantity = 20, Price = 8 },
+                new Inventory { InventoryId = 4, InventoryName = "Bike Pedal", Quantity = 20, Price = 1 }
+            );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product() { ProductId = 1, ProductName = "Bike", Quantity = 10, Price = 150 },
+                new Product() { ProductId = 2, ProductName = "Car", Quantity = 5, Price = 2500}
+            );
+
+            modelBuilder.Entity<ProductInventory>().HasData(
+                new ProductInventory { ProductInventoryId = 1, ProductId = 1, InventoryId = 1, InventoryQuantity = 1 }, // seat
+                new ProductInventory { ProductInventoryId = 2, ProductId = 1, InventoryId = 2, InventoryQuantity = 1 }, // body
+                new ProductInventory { ProductInventoryId = 3, ProductId = 1, InventoryId = 3, InventoryQuantity = 2 }, //wheels
+                new ProductInventory { ProductInventoryId = 4, ProductId = 1, InventoryId = 4, InventoryQuantity = 2 } //pedal
+            );
+        }
+    }
+}
--- IMS.WebApp/Program.cs
+++ IMS.WebApp/Program.cs
@@ -9,13 +9,52 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using IMS.UseCases.Reports.Interfaces;
 using IMS.UseCases.Reports;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using IMS.Plugins.EFCoreSqlServer;
+using IMS.UseCases.PluginInterfaces;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var constr = builder.Configuration.GetConnectionString("InventoryManagement");
+
+// configure authorizations
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Admin", policy => policy.RequireClaim("Department", "Administration"));
+    options.AddPolicy("Inventory", policy => policy.RequireClaim("Department", "InventoryManagement"));
+    options.AddPolicy("Sales", policy => policy.RequireClaim("Department", "Sales"));
+    options.AddPolicy("Purchasers", policy => policy.RequireClaim("Department", "Purchasing"));
+    options.AddPolicy("Productions", policy => policy.RequireClaim("Department", "ProductionManagement"));
+});
+
+//Configure EF Core for Identity
+builder.Services.AddDbContext<AccountDbContext>(options =>
+{
+    options.UseSqlServer(constr);
+});
+
+//Configure Identity
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedEmail = false;
+}).AddEntityFrameworkStores<AccountDbContext>();
+
+builder.Services.AddDbContextFactory<IMSContext>(options =>
+{
+    options.UseSqlServer(constr);
+});
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddTransient<IInventoryRepository, InventoryEFCoreRepository>();
+builder.Services.AddTransient<IProductRepository, ProductEFCoreRepository>();
+builder.Services.AddTransient<IInventoryTransactionRepository, InventoryTransactionEFCoreRepository>();
+builder.Services.AddTransient<IProductTransactionRepository, ProductTransactionEFCoreRepository>();
 
 
 builder.Services.AddTransient<IViewInventoriesByNameUseCase, ViewInventoriesByNameUseCase>();
@@ -53,7 +92,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
+
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+app.UseAuthentication();;
 
 app.Run();
